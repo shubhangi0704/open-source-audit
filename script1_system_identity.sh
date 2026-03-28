36156e4e6efe53c1757764561bce5dc3a0e9111e
shubhangi0704 <shubhangi.24boe10042@vitbhopal.ac.in>
2026-03-28 18:46:36 +0530

    Create script1_system_identity.sh

---
+++ script1_system_identity.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Variables using command substitution
+dist_name=$(grep '^PRETTY_NAME' /etc/os-release | cut -d= -f2 | tr -d '"')
+kernel=$(uname -r)
+user=$(whoami)
+home_dir=$HOME
+uptime=$(uptime -p)
+current_time=$(date)
+
+echo "======================================="
+echo "      SYSTEM IDENTITY REPORT           "
+echo "======================================="
+
+echo "Linux Distribution: $dist_name"
+echo "Kernel Version: $kernel"
+
+echo "Logged-in User: $user"
+echo "Home Directory: $home_dir"
+
+echo "System Uptime: $uptime"
+echo "Current Date & Time: $current_time"
+
+echo "---------------------------------------"
+echo "License Information:"
+echo "This system is primarily based on the GNU General Public License (GPL),"
+echo "which allows users to use, modify, and distribute software freely."
+echo "======================================="
